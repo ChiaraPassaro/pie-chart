3234b16e0795ba74ba53a51e0c276cd601a2a5a4
ChiaraPassaro <chiarapassaro.wd@gmail.com>
2023-11-21 07:11:22 +0100

    feat: added fake data, removed title

--- src/views/ChartsView.vue
+++ src/views/ChartsView.vue
@@ -4,25 +4,34 @@
 	//mockup data
 	const data = [
 		{
-			label: "Lorem",
-			quantity: 70,
+			label: "Label 1",
+			quantity: 20,
 			color: "hsl(20 60% 50%)",
 		},
 		{
-			label: "Ipsum Dolor",
-			quantity: 10,
+			label: "Label 2",
+			quantity: 20,
 			color: "hsl(180 60% 50%)",
 		},
 		{
-			label: "Sit",
+			label: "Label 3",
 			quantity: 20,
 			color: "hsl(200 60% 50%)",
+		},
+		{
+			label: "Label 4",
+			quantity: 20,
+			color: "hsl(220 60% 50%)",
+		},
+		{
+			label: "Label 5",
+			quantity: 20,
+			color: "hsl(240 60% 50%)",
 		},
 	]
 </script>
 <template>
 	<main>
-		<header><h2>Pie Chart</h2></header>
 		<PieChart v-if="data?.length" :data="data" />
 	</main>
 </template>
@@ -38,7 +47,7 @@
 	main {
 		display: flex;
 		flex-direction: column;
-		justify-content: space-between;
+		justify-content: center;
 		block-size: 100%;
 		padding: 2rem;
 	}
